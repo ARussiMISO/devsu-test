0a2d072e4ed5e13fe9a146ee73d856e3e93fedc8
Alejandro Russi <f.russi@uniandes.edu.co>
2024-06-11 05:31:47 +0000

    Manejo inicial de excepciones

---
+++ src/main/java/com/devsu/test/handlerException/exception/AlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.devsu.test.handlerException.exception;
+
+public class AlreadyExistsException extends Exception{
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/main/java/com/devsu/test/handlerException/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.devsu.test.handlerException.exception;
+
+public class NotFoundException extends Exception {
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
